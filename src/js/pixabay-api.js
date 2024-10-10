d5813a0622ca6dbfe5419ca5098c9c1b53d455a8
YD <yuriidiachuk@icloud.com>
2024-10-10 21:29:49 +0300

    second

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,8 @@
+export const BASE_URL = "https://portfolio-js.b.goit.study/api";
+import axios from 'axios';
+
+async function fetchReviews() {
+    const url = `${BASE_URL}`
+    const data = await axios.get("url")
+}
+   
